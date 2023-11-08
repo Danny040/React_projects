8830383a77a9f14289d4884bc7b148896d531f7c
Danny040 <dann0051@gmail.com>
2023-11-08 16:47:02 +0200

    everything is ready except local storage savings

--- ToDo/to_do_app/src/components/ToDoCard/ToDoCard.js
+++ ToDo/to_do_app/src/components/ToDoCard/ToDoCard.js
@@ -2,17 +2,32 @@ import './ToDoCard.css';
 import {FiEdit, FiCheckSquare} from 'react-icons/fi';
 import {IoTrashBinOutline} from 'react-icons/io5';
 
-function ToDoCard({toDoName, toDoDesc}) {
+function ToDoCard({toDoName, toDoDesc, todos, setTodos, todo}) {
+
+    const handleDelete = () => {
+        setTodos(todos.filter((el) => todo.id !== el.id));
+    }
+
+    const handleCheck = () => {
+        setTodos(todos.map((item)=>{
+            if(todo.id === item.id) {
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    }
+
     return (
         <div className='card-container'>
-            <div className='content'>
+            <div className={`content ${todo.completed ? 'done' : ''}`}>
                 <h2>{toDoName}</h2>
                 <p>{toDoDesc}</p>
             </div>
             <div className='options'>
-                <FiEdit className='edit' />
-                <FiCheckSquare className='completed' />
-                <IoTrashBinOutline className='delete' />
+                <FiCheckSquare onClick={handleCheck} className='completed' />
+                <IoTrashBinOutline onClick={handleDelete} className='delete' />
             </div>
         </div>
     );
--- ToDo/to_do_app/src/components/ToDoList/ToDoList.js
+++ ToDo/to_do_app/src/components/ToDoList/ToDoList.js
@@ -1,13 +1,12 @@
 import './ToDoList.css';
 import ToDoCard from '../ToDoCard/ToDoCard.js';
 
-function ToDoList({todos}) {
+function ToDoList({todos, setTodos}) {
     return (
         <div className='list-wrapper'>
             {todos.map((item)=>{
-                console.log(item);
                 return (
-                    <ToDoCard key={item.id} toDoName={item.name} toDoDesc={item.description} />
+                    <ToDoCard key={item.id} todo={item} toDoName={item.name} toDoDesc={item.description} todos={todos} setTodos={setTodos} />
                 );
             })}
         </div>
